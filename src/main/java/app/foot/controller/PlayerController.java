fb826b3ee149dad206d003770af02bb7750baec8
HARIVOLA Andy <hei.andy.8@gmail.com>
2023-02-08 18:49:48 +0300

    initial commit

---
+++ src/main/java/app/foot/controller/PlayerController.java
@@ -0,0 +1,44 @@
+package app.foot.controller;
+
+import app.foot.controller.rest.Player;
+import app.foot.controller.rest.mapper.PlayerRestMapper;
+import app.foot.service.PlayerService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@AllArgsConstructor
+public class PlayerController {
+    private final PlayerRestMapper mapper;
+    private final PlayerService service;
+
+    @GetMapping("/players")
+    public List<Player> getPlayers() {
+        return service.getPlayers().stream()
+                .map(mapper::toRest)
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    @PostMapping("/players")
+    public List<Player> addPlayers(@RequestBody List<Player> toCreate) {
+        List<app.foot.model.Player> domain = toCreate.stream()
+                .map(mapper::toDomain)
+                .collect(Collectors.toUnmodifiableList());
+        return service.createPlayers(domain).stream()
+                .map(mapper::toRest)
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    @PutMapping("/players")
+    public List<Player> modifyPlayer(@RequestBody List<Player> toCreate) {
+        List<app.foot.model.Player> domain = toCreate.stream()
+                .map(mapper::toDomain)
+                .collect(Collectors.toUnmodifiableList());
+        return service.updatePlayers(domain).stream()
+                .map(mapper::toRest)
+                .collect(Collectors.toUnmodifiableList());
+    }
+}
---
+++ src/main/java/app/foot/service/PlayerService.java
@@ -0,0 +1,59 @@
+package app.foot.service;
+
+import app.foot.controller.validator.PlayerValidator;
+import app.foot.exception.BadRequestException;
+import app.foot.model.Player;
+import app.foot.repository.PlayerRepository;
+import app.foot.repository.entity.PlayerEntity;
+import app.foot.repository.mapper.PlayerMapper;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class PlayerService {
+    private final PlayerRepository repository;
+    private final PlayerMapper mapper;
+    private final PlayerValidator validator;
+
+    public List<Player> getPlayers() {
+        return repository.findAll().stream()
+                .map(mapper::toDomain)
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    public List<Player> createPlayers(List<Player> toCreate) {
+        return repository.saveAll(toCreate.stream()
+                        .map(mapper::toEntity)
+                        .collect(Collectors.toUnmodifiableList())).stream()
+                .map(mapper::toDomain)
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    @Transactional
+    public List<Player> updatePlayers(List<Player> toUpdate) {
+        for (int i = 0; i < toUpdate.size(); i++) {
+            validator.accept(toUpdate.get(i));
+            int IdOfI = i;
+            PlayerEntity playerActual = repository.findById(toUpdate.get(i).getId()).orElseThrow(
+                    ()->{throw new BadRequestException("can't update new id "+ toUpdate.get(IdOfI).getId());}
+            );
+            if ( !toUpdate.get(i).getTeamName().equals(mapper.toDomain(playerActual).getTeamName())) {
+                throw new BadRequestException("Team can't change in Player with id:"+toUpdate.get(i).getId());
+            }
+            if (toUpdate.get(i).getId() == null) {
+                throw new BadRequestException("Error");
+            }
+        }
+        return repository.saveAll(toUpdate.stream()
+                        .map(mapper::toEntity)
+                        .collect(Collectors.toUnmodifiableList()))
+                .stream()
+                .map(mapper::toDomain)
+                .collect(Collectors.toUnmodifiableList());
+    }
+}
